40d6b4fa8353c42a5c915bb87950ced4aead5e39
Stefano Casasola <ste.casasola@gmail.com>
2025-06-12 10:51:55 +0200

    Fills the calendar and creates data structure

--- js/script.js
+++ js/script.js
@@ -7,6 +7,25 @@ const now = new Date() // new Date() crea un OGGETTO di tipo DATA in JS
 // le parti mancanti del calendario all'avvio della pagina
 // console.log(now)
 
+// gli appuntamenti nel calendario saranoo salvati così: un ARRAY di ARRAY (di stringhe)
+// [
+//   ["15:00 - Dentista", "18:00 - Cinema", "23:00 - Nanna"]
+//   ["15:00 - Dentista", "18:00 - Cinema", "23:00 - Nanna"]
+//   ["15:00 - Dentista", "18:00 - Cinema", "23:00 - Nanna"]
+//   ["15:00 - Dentista", "18:00 - Cinema", "23:00 - Nanna"]
+// ]
+
+const appointments = [] // è la cassettiera, che si deve riempire di cassettini (1 x gg)
+// dopo appointments deve diventare così:
+// [
+//   [], [], [], [], [],
+//   [], [], [], [], [],
+//   [], [], [], [], [],
+//   [], [], [], [], [],
+//   [], [], [], [], [],
+//   [], [], [], [], [],
+// ]
+
 const monthArray = [
   'Gennaio', // 0
   'Febbraio', // 1
@@ -43,7 +62,50 @@ const numberOfDaysInCurrentMonth = function () {
   // da quella data 1gg.
   // Avremo così trovato l'ULTIMO GIORNO del mese corrente: il numero di quel giorno è
   // anche il numero dei giorni del mese.
+  // dobbiamo calcolare la data del primo giorno del mese prossimo (e toglierci un gg)
+  const currentYear = now.getFullYear() // 2025
+  const currentMonth = now.getMonth() // 5
+
+  // vado a calcolare con queste informazioni il primo gg del mese prossimo
+  // new Date(currentYear, currentMonth + 1, 1) // 1 Luglio 2025
+  const lastDayOfTheMonth = new Date(currentYear, currentMonth + 1, 0) // 0 Luglio 2025 -> 30 Giugno 2025
+  console.log(lastDayOfTheMonth) // ora dà il 30 giugno 2025
+  // ora estraggo solamente il giorno del mese
+  return lastDayOfTheMonth.getDate() // 30
 }
 
 printCurrentMonthInH1()
-numberOfDaysInCurrentMonth()
+// numberOfDaysInCurrentMonth() // questa funzione se eseguita torna il NUMERO dei giorni del mese corrente
+
+// abbiamo capito quanti giorni ha il mese corrente: questo numero ci servirà
+// per capire QUANTE CELLE CREARE nel nostro calendario!
+// vado a creare una funzione che riempirà la section con id "calendar" con un numero
+// di caselle pari al numero dei giorni del mese (numberOfDaysInCurrentMonth())
+const createCells = function () {
+  // quante celle devo creare? devo creare numberOfDaysInCurrentMonth() celle
+
+  // prendo un riferimento alla section ancora vuota
+  const calendarSection = document.getElementById('calendar')
+  // creo una cella e l'appendo al suo interno numberOfDaysInCurrentMonth() volte
+
+  // calcoliamo il limite per il for, ovvero quante caselle deve creare
+  // ne deve creare una per OGNI GIORNO del mese corrente
+  const numberOfTotalDays = numberOfDaysInCurrentMonth()
+
+  for (let i = 0; i < numberOfTotalDays; i++) {
+    // nel nostro caso di oggi è come dire let i=0; i<30; i++
+    // per ogni giorno del mese, creo una cella
+    const dayCell = document.createElement('div') // <div></div>
+    dayCell.classList.add('day') // <div class="day"></div>
+    const dayCellValue = document.createElement('h3') // <h3></h3>
+    dayCellValue.innerText = i + 1 // per fare le celle dall'1 al 30, es. <h3>1</h3>
+    dayCell.appendChild(dayCellValue) // <div class="day"><h3>1</h3></div>
+    calendarSection.appendChild(dayCell)
+
+    // ora pushiamo un cassettino nella cassettiera degli appuntamenti
+    appointments.push([]) // un array vuoto dentro l'array degli appuntamenti
+  }
+  console.log(appointments)
+}
+
+createCells()
